3e79d1b6df02c55e877e5eb23b7a7adadf889113
Jonathan-dev2002 <jonathandoillon2002@gmail.com>
2025-11-28 01:33:50 +0700

    feat(mapper): implement VehicleMapper for entity-to-DTO transformation

--- src/main/java/com/miniProject/Carpool/mapper/VehicleMapper.java
+++ src/main/java/com/miniProject/Carpool/mapper/VehicleMapper.java
@@ -1,4 +1,30 @@
 package com.miniProject.Carpool.mapper;
 
+import com.miniProject.Carpool.dto.VehicleResponse;
+import com.miniProject.Carpool.model.Vehicle;
+import org.springframework.stereotype.Component;
+
+@Component
 public class VehicleMapper {
+
+    public VehicleResponse toResponse(Vehicle vehicle) {
+        if(vehicle ==null) return null;
+
+        VehicleResponse res = new VehicleResponse();
+        res.setId(vehicle.getId());
+        res.setUserId(vehicle.getUser().getId());
+        res.setLicensePlate(vehicle.getLicensePlate());
+        res.setVehicleModel(vehicle.getVehicleModel());
+        res.setVehicleType(vehicle.getVehicleType());
+        res.setColor(vehicle.getColor());
+        res.setSeatCapacity(vehicle.getSeatCapacity());
+        res.setAmenities(vehicle.getAmenities());
+        res.setPhotos(vehicle.getPhotos());
+        res.setIsDefault(vehicle.getIsDefault());
+
+        res.setCreatedAt(vehicle.getCreatedAt());
+        res.setUpdatedAt(vehicle.getUpdatedAt());
+
+        return res;
+    }
 }
