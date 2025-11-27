418b1edeb251ac8634fa737550540bcf5e47c662
Jonathan-dev2002 <jonathandoillon2002@gmail.com>
2025-11-28 01:32:10 +0700

    feat(repo): add VehicleRepository with JpaSpecificationExecutor support

--- src/main/java/com/miniProject/Carpool/repository/VehicleRepository.java
+++ src/main/java/com/miniProject/Carpool/repository/VehicleRepository.java
@@ -1,4 +1,17 @@
 package com.miniProject.Carpool.repository;
 
-public class VehicleRepository {
+import com.miniProject.Carpool.model.DriverVerification;
+import com.miniProject.Carpool.model.User;
+import com.miniProject.Carpool.model.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface VehicleRepository extends JpaRepository<Vehicle, String> , JpaSpecificationExecutor<Vehicle> {
+//    Optional<Vehicle> findVehicleByIsDefault(User user,Boolean isDefault);
+    Optional<Vehicle> findByUserAndIsDefaultTrue(User user);
+    Optional<Vehicle> findByIdAndUserId(String id, String userId);
 }
