853eb81ec385c9ffcdf4e7c1e813d1f0f68367f3
Jonathan-dev2002 <jonathandoillon2002@gmail.com>
2025-11-28 14:45:03 +0700

    feat(repo): add NotificationRepository with unread count and batch mark-read support

--- src/main/java/com/miniProject/Carpool/repository/NotificationRepository.java
+++ src/main/java/com/miniProject/Carpool/repository/NotificationRepository.java
@@ -1,4 +1,19 @@
 package com.miniProject.Carpool.repository;
 
-public class NotificationRepository {
+import com.miniProject.Carpool.model.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, String>, JpaSpecificationExecutor<Notification> {
+    long countByUserIdAndReadAtIsNull(String userId); // Count Unread
+
+    // Mark All Read (Batch Update)
+    // @Query จะเขียนคำสั่ง SQL (หรือ JPQL)
+    @Modifying
+    @Query("UPDATE Notification n SET n.readAt = CURRENT_TIMESTAMP WHERE n.user.id = :userId AND n.readAt IS NULL") //CURRENT_TIMESTAMP เป็นฟังก์ชัน ที่จะไปดึงเวลาปัจจุบันจาก db มาใส่
+    void markAllReadByUserId(String userId);
 }
